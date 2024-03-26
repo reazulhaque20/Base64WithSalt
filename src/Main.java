aac6a46d9233711abed40b6875f6c37529992af6
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2024-03-26 23:55:49 +0800

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,26 @@
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
-
 import java.util.Date;
 import java.util.Random;
 import java.io.IOException;
 
 public class Main {
     private static final Random rand = new Random((new Date()).getTime());
-
+    
     public static void main(String[] args) {
         String st = "secrete";
-
         String enc = encrypt(st);
-
         System.out.println("Encrypted string :" + enc);
-
         System.out.println("Decrypted string :" + decrypt(enc));
     }
     public static String encrypt(String str) {
-
         BASE64Encoder encoder = new BASE64Encoder();
-
         byte[] salt = new byte[8];
-
         rand.nextBytes(salt);
-
         return encoder.encode(salt) + encoder.encode(str.getBytes());
     }
 
     public static String decrypt(String encStr) {
-
         if (encStr.length() > 12) {
             String cipher = encStr.substring(12);
             BASE64Decoder decoder = new BASE64Decoder();
